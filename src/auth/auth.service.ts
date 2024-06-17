9f2d6b15885ce289e176fbb826a558f12a8dbcea
Gmkeiner <munozpallareskeinerjose@gmail.com>
2024-06-17 14:52:10 -0500

    se uso JWT ya con el password encriptado queda pendiente colocarle mas cambios

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/auth.entity';
 import { Repository } from 'typeorm';
 import { AuthRegisterDto } from './dto/auth-register.dto';
-
+import * as bcrypt from 'bcryptjs';
 
 
 @Injectable()
@@ -13,12 +13,27 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>
   ){}
-
+  
   async signUp(authRegisterDto: AuthRegisterDto){
-
     const { username, email, password } = authRegisterDto;
 
-    const userAuth = await this.userRepository.create({username,email,password});
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+
+        // console.log('salt', salt);
+        // console.log('hash', hash);
+
+    const userAuth = await this.userRepository.create({username, email, password: hashPassword});
+
+        //  console.log(userAuth);
+        //  return 'Hola'
+
+    const getUser = await this.userRepository.findOne({
+      where: {
+        email
+      }
+    })
+    if ( getUser ) throw new ConflictException('Email already exists');
 
     return await this.userRepository.save(userAuth);
   }
